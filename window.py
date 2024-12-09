bdef1f56a68b4c659f9c0df4f5d0b27279af53f0
M2Pss <151238316+M2Pss@users.noreply.github.com>
2024-12-08 18:35:12 -0800

    Update window.py

--- window.py
+++ window.py
@@ -1,24 +1,36 @@
-from customtkinter import CTk, CTkCanvas
-from background import Background
+from customtkinter import CTkCanvas
 from pacman import Pacman
+from map import Map
 
 class Window:
+    """
+    Class to manage the main window
+    """
     def __init__(self, master):
+        """
+        Initialisation of the class
+
+        Args:
+            master (tkinter): main tkinter window
+        """
         self.master = master
         self.show_game()
         
     def show_game(self):
+        """
+        Function to display the game
+        """
         master_width, master_height = 600, 600
         self.master.geometry(f"{master_width}x{master_height}")
 
         main_canvas = CTkCanvas(self.master, width=master_width, height=master_height, bg="black")
         main_canvas.pack(fill="both", expand=True)
-
-        background = Background(main_canvas)
-        background.display_background(master_width, master_height)
-
-        pacman = Pacman(main_canvas, 200, 200)
+    
+        #background = Background(main_canvas, master_width, master_height)
+        #background.display_background()
+        
+        map_ = Map(main_canvas)
+        
+        pacman = Pacman(main_canvas, 375, 375, map_)
 
         self.master.bind("<KeyPress>", pacman.move)
-
-        self.master.mainloop()
